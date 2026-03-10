3ac01eebe463cf8abe1d99d5ac736a2820d51978
divya <divyayadav1458@gmail.com>
2026-03-10 23:19:06 +0530

    array one problem

--- Day6_Arrays/problem_25.java
+++ Day6_Arrays/problem_25.java
@@ -1,5 +1,58 @@
 package Day6_Arrays;
 
+import java.util.Scanner;
+
 public class problem_25 {
-    
+
+    // two sum approach O(n^2)
+    public int[] twoSum(int[] nums, int target) {
+
+        int n = nums.length;
+        int[] solarr = new int[2];
+
+        for(int i = 0; i < n; i++){
+
+            int sum = nums[i];
+
+            for(int j = i + 1; j < n; j++){
+
+                sum = nums[i] + nums[j];
+
+                if(sum == target){
+                    solarr[0] = i;
+                    solarr[1] = j;
+                    return solarr;
+                }
+
+            }
+        }
+
+        return solarr;
+    }
+
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        problem_25 obj = new problem_25();
+
+        int[] result = obj.twoSum(nums, target);
+
+        System.out.println("Indices are: " + result[0] + " " + result[1]);
+    }
 }
