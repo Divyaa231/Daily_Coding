cde9446c880c510feb45257ac2db3981c7857edd
divya <divyayadav1458@gmail.com>
2026-03-16 23:54:37 +0530

    Array medium Question

---
+++ Day6_Arrays/Array_medium/problem_3O.java
@@ -0,0 +1,28 @@
+package Day6_Arrays.Array_medium;
+
+public class problem_3O {
+    //maximum subarray sum
+    public static int maxSubArray(int[] nums) {
+         int n = nums.length;
+        int max  = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i =0;i<n;i++){
+           
+            sum += nums[i];
+
+            if(sum>max){
+                max = sum;
+            }
+
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        return max;     
+    }    
+
+public static void main(String[] args) {
+        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(arr));
+    }
+}
